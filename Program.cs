e206ebc3a692138ca21123677cb6e76253ea6751
ChandraAwesome <120436175+ChandraAwesome@users.noreply.github.com>
2023-02-02 17:12:09 +0530

    [Chandra]- "Regex Programs"

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using Regexx;
+
+namespace Regexx
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            pincode check= new pincode();
+            Console.WriteLine(check.checkPincode("400088"));
+            Console.WriteLine(check.checkPincode("A400088"));
+        }
+    }
+}
---
+++ pincode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Regexx
+{
+   public class pincode
+    {
+        public static string PINCODE = "^[1-9][0-9]{5}$";
+        public static string PINCODE_ = "^[a-c]@[a-z].[com]$";
+   
+
+        public bool checkPincode(string pincodee)
+        {
+            return Regex.IsMatch(pincodee, PINCODE);
+         
+        }
+    }
+}
